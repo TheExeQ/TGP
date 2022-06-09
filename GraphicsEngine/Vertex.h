6d0d4df340b60645788b61cbd7635cac2d8fe60e
Samuel Ryberg <samuel.ryberg@gmail.com>
2022-06-09 09:44:57 +0200

    yey

--- GraphicsEngine/Vertex.h
+++ GraphicsEngine/Vertex.h
@@ -14,12 +14,15 @@ struct Vertex
 		VertexColors[0] = { 0.f, 0.f, 0.f, 1.f };
 	};
 	Vertex(float x, float y, float z, float r, float g, float b, float a,
+		float u, float v,
 		Vector3<float> aTangent, Vector3<float> aBinormal, Vector3<float> aNormal)
 	{
 		Position.x = x;
 		Position.y = y;
 		Position.z = z;
 		VertexColors[0] = { r, g, b, a };
+
+		UVs[0] = { u, v };
 
 		Tangent = aTangent;
 		Binormal = aBinormal;
