26cd69144922e056c4b7dbf43ef888cdd17fa40a
samuel.ryberg <samuel.ryberg@gmail.com>
2022-09-01 10:58:35 +0200

    added yaml and thirdparty project

--- ThirdParty/premake5.lua
+++ ThirdParty/premake5.lua
@@ -1,4 +1,4 @@
-project "GraphicsEngine"
+project "ThirdParty"
 	kind "StaticLib"
 	language "C++"
 	
@@ -6,24 +6,17 @@ project "GraphicsEngine"
 	targetname ("%{prj.name}-%{cfg.buildcfg}")
 	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
 
-	postbuildcommands 
-	{
-		"{COPY} ../ThirdParty/FBXImporter/lib/libfbxsdk.dll ../bin/"
-	}
-
 	files
 	{
 		"./**.h",
 		"./**.hpp",
+		"./**.c",
 		"./**.cpp",
-		"./**.hlsl",
-		"./**.hlsli",
 	}
 	
 	includedirs
 	{
 		"./",
-		"../ThirdParty/"
 	}
 	
 	libdirs
@@ -31,24 +24,6 @@ project "GraphicsEngine"
 		"../libs",
 		"../ThirdParty/FBXImporter/lib"
 	}
-
-	links
-	{
-		"ThirdParty"
-	}
-	
-	filter { "files:**.hlsl" }
-		shadermodel "5.0"
-		shaderobjectfileoutput "../Assets/Shaders/%{file.basename}.cso"
-
-	filter { "files:**VS.hlsl" }
-		shadertype "Vertex"
-
-	filter { "files:**PS.hlsl" }
-		shadertype "Pixel"
-		
-	filter { "files:**GS.hlsl" }
-		shadertype "Geometry"
 
 	filter "system:windows"
 		cppdialect "C++20"
@@ -65,12 +40,30 @@ project "GraphicsEngine"
 		defines "DEBUG"
 		symbols "On"
 		runtime "Debug"
-
+		
+		links
+		{
+			"d3d11.lib",
+			"TGAFBXImporterd.lib"
+		}
+		
 	filter "configurations:Release"
 		defines "RELEASE"
 		symbols "On"
 		runtime "Release"
 		
+		links
+		{
+			"d3d11.lib",
+			"TGAFBXImporter.lib"
+		}
+		
 	filter "configurations:Retail"
 		defines "RETAIL"
 		symbols "On"
+		
+		links
+		{
+			"d3d11.lib",
+			"TGAFBXImporter.lib"
+		}
--- premake5.lua
+++ premake5.lua
@@ -12,4 +12,5 @@ workspace "TGP"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "GraphicsEngine/"
+include "ThirdParty/"
 include "Modelviewer/"
